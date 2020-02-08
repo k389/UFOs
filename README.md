90e68d772427476c8752fb3507dc7f73babcf7d6
k389 <knushik@gmail.com>
2020-02-08 16:01:44 -0500

    Adding UFO files practice and challenge

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # UFOs
-JavaScript, Bootstrap
+JavaScript, Bootstrap, CSS, VS Code
+
+# Project Overview/Challenge
+- Create, update, and diploy JavaScript functions. 
+	- filters include date, city, statem country and shape from the data.
+- Update and deploy forEach(for loop) to loop through the filters and update the table based on user input.
+- Update and populate the dynamic filters and table using JavaScript and HTML.
+
+# Resources
+- Data Source: data.js
+- Software: Visual Studio Code, 1.40.2., Google Chrome, JavaScript, Bootstrap
+
+# Summary
+- Practice
+	- Used VS Code to create JavaSpript and HTML to deploy UFO page. 
+	- UFO HTML page displays a header with image, an article and a data table.
+	- The data table displays data from data.js using JavaScript function.
+	- The date filter is created to filter the data table based on user date entry.
+	- the date filter is created using JavaScript function. 
+	- JavaScript methods used are:
+		- forEach to create the data table.
+		- if else statement to filter the data table based on the date.
+		- d3.selectAll().on() to listen to the form button 
+	- Customized the HTML page using style.css to display image in the header, to change the background and text color and align text.
+- Challenge
+	- Updated the filters to add additional filters besides date - city, state, country and shape.
+		- updated the HTML to include the additional filters to the list. 
+		- updated the app.js code to be able to use the additional filters to filter through the data.
+	- Created new function to filter the table
+		- used forEach to loop through the filters and display data that matches the filter values.
+	- Used d3.selectAll().on() to attach event listener to pick up changes and use the Filter Table button.
+- Further Recommendation
+	- The page is using of data source (data.js), which can be limited. 
+	- the developer can update the page by finding news articles (other current resources) and scarape the data to display on the page. This will give the most current infromation on the topic.
+	- the developer also can also can customize the page to have the background change every time it is refreshed instead of having one background image. 
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,45 +1,76 @@
-// import the data from data.js
+// from data.js - the data
 const tableData = data;
-
-// Reference the HTML table using d3
+// get table references - from HTML
 var tbody = d3.select("tbody");
 
-//building a table
 function buildTable(data) {
-    // First, clear out any existing data
-    tbody.html("");
-    // Next, loop through each object in the data
-    // and append a row and cells for each value in the row
-    data.forEach((dataRow) => {
-      // Append a row to the table body
-      let row = tbody.append("tr");
-      // Loop through each field in the dataRow and add
-      // each value as a table cell (td)
-      Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
-        }
-      );
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
     });
-  }
-//function to create an if statement 
-function handleClick() {
-    // Grab the datetime value from the filter
-    let date = d3.select("#datetime").property("value");
-    let filteredData = tableData;
-    // Check to see if a date was entered and filter the
-    // data using that date.
-    if (date) {
-      // Apply `filter` to the table data to only keep the
-      // rows where the `datetime` value matches the filter value
-      filteredData = filteredData.filter(row => row.datetime === date);
-    };
-    // Rebuild the table using the filtered data
-    // @NOTE: If no date was entered, then filteredData will
-    // just be the original tableData.
-    buildTable(filteredData);
-  }
-// Attach an event to listen for the form button
-d3.selectAll("#filter-btn").on("click", handleClick);
+  });
+}
+
+//buildTable(tableData)
+// Keep track of all filters
+var filters = {};
+
+// This function will replace your handleClick function
+function updateFilters() {
+
+  // Save the element, value, and id of the filter that was changed
+      let changeditem = d3.select(this);
+      let changedvalue = changeditem.property("value");
+      let changedid = changeditem.attr("id");
+
+  // console.log("-----------------")
+  // console.log(changeditem)
+  // console.log(changedvalue)
+  // console.log(changedid)
+  // console.log("----------------")
+  // If a filter value was entered then add that filterId and value to the filters list. 
+  
+if (changedvalue) {
+    filters[changedid] = changedvalue;
+}
+//Otherwise, clear that filter from the filters object
+else {
+  delete filters[changedid];
+};
+  // Call function to apply all filters and rebuild the table
+  filterTable(filters);
+}
+
+function filterTable() {
+  
+  // Set the filteredData to the tableData
+  let filteredData = tableData;
+  // Loop through all of the filters and keep any data that  matches the filter values
+  Object.entries(filters).forEach(([key, value]) => {
+    filteredData = filteredData.filter(row => row[key] === value)
+  });
+
+  // Finally, rebuild the table using the filtered Data
+  buildTable(filteredData);
+}
+
+
+// Attach an event to listen for changes to each filter
+// Hint: You'll need to select the event and what it is listening for within each set of parenthesis
+d3.selectAll("#filter-btn").on("click", updateFilters);
+d3.selectAll("input").on("change", updateFilters);
+//d3.selectAll("#filter-btn").on("click", handleClick);
+
+
 // Build the table when the page loads
 buildTable(tableData);
+
